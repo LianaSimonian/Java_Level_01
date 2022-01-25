af2b2042a75e7696f071119503e97059a569c8b4
LianaSimonian <liana.simoniian@gmail.com>
2022-01-25 19:53:03 +0300

    homework5

---
+++ src/com/aca/homework/week2/user/creation/User.java
@@ -0,0 +1,16 @@
+package com.aca.homework.week2.user.creation;
+
+public class User {
+    String name;
+    int height;
+
+    User() {
+
+    }
+
+    User(String str, int h) {
+        name = str;
+        height = h;
+
+    }
+}
---
+++ src/com/aca/homework/week2/user/creation/UserCreation.java
@@ -0,0 +1,23 @@
+package com.aca.homework.week2.user.creation;
+
+public class UserCreation {
+    public static void printUser(User user) {
+
+        System.out.println("The name is:" + user.name + ", Marry's height is:" + user.height);
+    }
+
+    public static void main(String[] args) {
+
+        //User[] users1 = {new User("m",2),new User("k",1),new User("K",4)};
+
+        User[] users = new User[3];
+
+        users[0] = new User("Marry", 170);
+        users[1] = new User("Grace", 169);
+        users[2] = new User("Nicolas", 175);
+
+        for (int i = 0; i < users.length; i++) {
+            printUser(users[i]);
+        }
+    }
+}
