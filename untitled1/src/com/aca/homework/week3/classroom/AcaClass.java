81d93754797ba96bb9bf7c8944e984210f2b425b
LianaSimonian <liana.simoniian@gmail.com>
2022-01-26 14:33:44 +0300

    The classroom solution

---
+++ untitled1/src/com/aca/homework/week3/classroom/AcaClass.java
@@ -0,0 +1,21 @@
+package com.aca.homework.week3.classroom;
+
+public class AcaClass {
+
+    Student[] students;
+    long startMillis;
+    long endMillis;
+    String name;
+    long fee;
+
+    public void print() {
+        System.out.println("AcaClass Name: " + name);
+        System.out.println("AcaClass fee: " + fee);
+        System.out.println("AcaClass start time by millis: " + startMillis);
+        System.out.println("AcaClass end time by millis: " + endMillis);
+        System.out.println("AcaClass name  surname  of students: ");
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("name:" + students[i].name + " surname :" + students[i].surname);
+        }
+    }
+}
