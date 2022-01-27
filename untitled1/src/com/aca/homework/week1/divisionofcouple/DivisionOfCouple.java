495a97c26b6bb1d9f3adf1e576c6b23731a57c28
LianaSimonian <liana.simoniian@gmail.com>
2022-01-27 16:02:18 +0300

    The Division of couple program

---
+++ untitled1/src/com/aca/homework/week1/divisionofcouple/DivisionOfCouple.java
@@ -0,0 +1,20 @@
+package com.aca.homework.week1.divisionofcouple;
+
+import java.util.Scanner;
+
+public class DivisionOfCouple {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        int c = 0;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("input arr[" + i + "] ");
+            Scanner sc = new Scanner(System.in);
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++)
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.println("division of (" + i + "," + j + ") couple is equal to " + arr[i] / arr[j]);
+            }
+
+    }
+}
