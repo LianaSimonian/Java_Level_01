bb2931a25606e5708b4e1c6b976ea097bae40561
LianaSimonian <liana.simoniian@gmail.com>
2022-02-01 20:26:19 +0300

    first exam

--- untitled1/src/com/aca/homework/week3/stack/strings/Main.java
+++ untitled1/src/com/aca/homework/week3/stack/strings/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Stack stack = new Stack();
 
-        try {
             System.out.println("is stack empty?:" + stack.isEmpty());
             stack.push("main");
             stack.push("push");
@@ -17,9 +16,7 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 System.out.println("removed value:"+ stack.pop());
             }
-        }catch(Exception e){
-            System.out.println(e);
-        }
+
         System.out.println("size:" + stack.sizeOfStack());
     }
 }
