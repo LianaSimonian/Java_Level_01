3976506cd514e26a4a2d0e7e320f1da252d8a6d0
LianaSimonian <liana.simoniian@gmail.com>
2022-02-02 12:21:58 +0300

    The solution of the time

---
+++ untitled1/src/com/aca/homework/week4/time/Time.java
@@ -0,0 +1,62 @@
+package com.aca.homework.week4.time;
+
+public class Time {
+    private long seconds;
+
+    public Time() {
+        seconds = 0;
+    }
+
+    public Time(long seconds) {
+        this.seconds = seconds - (seconds / (24 * 60 * 60)) * (24 * 60 * 60);
+    }
+
+    public long getSeconds() {
+        return seconds - getHours() * 3600 - getMinutes() * 60;
+    }
+
+    public long getMinutes() {
+        return (seconds - getHours() * 3600) / 60;
+    }
+
+    public long getHours() {
+        return seconds / 3600;
+    }
+
+    public void print() {
+        long hours = getHours();
+        long minutes = getMinutes();
+        long seconds = getSeconds();
+        if (hours >= 0 && hours <= 9) {
+            if (minutes >= 0 && minutes <= 9) {
+                if (seconds >= 0 && seconds <= 9) {
+                    System.out.println("0" + hours + ":0" + minutes + ":0" + seconds);
+                } else {
+                    System.out.println("0" + hours + ":0" + minutes + ":" + seconds);
+                }
+            } else {
+                if (seconds >= 0 && seconds <= 9) {
+                    System.out.println("0" + hours + ":" + minutes + ":0" + seconds);
+                } else {
+                    System.out.println("0" + hours + ":" + minutes + ":" + seconds);
+                }
+
+            }
+        } else {
+            if (minutes >= 0 && minutes <= 9) {
+                if (seconds >= 0 && seconds <= 9) {
+                    System.out.println(hours + ":0" + minutes + ":0" + seconds);
+                } else {
+                    System.out.println(hours + ":0" + minutes + ":" + seconds);
+                }
+            } else {
+                if (seconds >= 0 && seconds <= 9) {
+                    System.out.println(hours + ":" + minutes + ":0" + seconds);
+                } else {
+                    System.out.println(hours + ":" + minutes + ":" + seconds);
+                }
+
+            }
+        }
+    }
+}
---
+++ untitled1/src/com/aca/homework/week4/time/Time_Test.java
@@ -0,0 +1,20 @@
+package com.aca.homework.week4.time;
+
+public class Time_Test {
+    public static void main(String[] args) {
+        Time time = new Time();
+        time.print();
+
+        Time time0 = new Time(2 * 3600 + 5 * 60 + 6);
+        time0.print();
+
+        Time time1 = new Time(14 * 3600 + 5 * 60 + 6);
+        time1.print();
+
+        Time time2 = new Time(49 * 3600 + 10 * 60 + 15);
+        time2.print();
+
+        Time time3 = new Time(24 * 60 * 60);
+        time3.print();
+    }
+}
