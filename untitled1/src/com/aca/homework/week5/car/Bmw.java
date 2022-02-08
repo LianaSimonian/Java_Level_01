84efef1741b7d402af88a6f5b419ac57514e3441
LianaSimonian <97042938+LianaSimonian@users.noreply.github.com>
2022-02-09 00:33:10 +0300

    Delete untitled1/src/com/aca/homework/week5/car directory

--- untitled1/src/com/aca/homework/week5/car/Bmw.java
+++
@@ -1,8 +0,0 @@
-package com.aca.homework.week5.car;
-
-public class Bmw extends Car {
-    public Bmw(String color, long width, long weight) {
-        super(color, width, weight, "bmw");
-    }
-
-}
--- untitled1/src/com/aca/homework/week5/car/Hyundai.java
+++
@@ -1,7 +0,0 @@
-package com.aca.homework.week5.car;
-
-public class Hyundai extends Car {
-    Hyundai(String color, long width, long weight) {
-        super(color, width, weight, "hyundai");
-    }
-}
