2d95eef7210b67d0b282327878c991295e98349e
LianaSimonian <liana.simoniian@gmail.com>
2022-02-09 00:44:59 +0300

    The solution of the polymorphic car problem

---
+++ untitled1/src/com/aca/homework/week5/car/Bmw.java
@@ -0,0 +1,9 @@
+package com.aca.homework.week5.car;
+
+public class Bmw extends Car {
+
+    public Bmw(String color, long width, long weight) {
+        super(color, width, weight, "bmw");
+    }
+
+}
---
+++ untitled1/src/com/aca/homework/week5/car/Hyundai.java
@@ -0,0 +1,9 @@
+package com.aca.homework.week5.car;
+
+public class Hyundai extends Car {
+
+    Hyundai(String color, long width, long weight) {
+        super(color, width, weight, "hyundai");
+    }
+
+}
