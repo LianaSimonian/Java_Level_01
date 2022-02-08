59376fcbbdc181f3322150ced9845b5a8e9b7e97
LianaSimonian <liana.simoniian@gmail.com>
2022-02-08 23:57:05 +0300

    The solution of the polymorphic car problem

--- untitled1/src/com/aca/homework/week5/car/CarTester.java
+++ untitled1/src/com/aca/homework/week5/car/CarTester.java
@@ -11,24 +11,27 @@ public class CarTester {
     public static Car[] create5CarsObject() {
         Car[] cars = new Car[5];
         for (int i = 0; i < 5; i++) {
-            System.out.println("input  color");
-            String color = new Scanner(System.in).nextLine();
-            System.out.println("input width");
-            long width = new Scanner(System.in).nextLong();
-            System.out.println("input weight");
-            long weight = new Scanner(System.in).nextLong();
-            System.out.println("input brand");
-            String brand = new Scanner(System.in).nextLine();
-            if (brand.equalsIgnoreCase("bmw")) {
-                cars[i] = new Bmw(color, width, weight);
-            }
-            if (brand.equalsIgnoreCase("hyundai")) {
-                cars[i] = new Hyundai(color, width, weight);
-            }
+            cars[i]=createCarObject();
         }
         return cars;
     }
-
+    public static Car createCarObject(){
+        System.out.println("input  color");
+        String color = new Scanner(System.in).nextLine();
+        System.out.println("input width");
+        long width = new Scanner(System.in).nextLong();
+        System.out.println("input weight");
+        long weight = new Scanner(System.in).nextLong();
+        System.out.println("input brand");
+        String brand = new Scanner(System.in).nextLine();
+        if (brand.equalsIgnoreCase("bmw")) {
+           return new Bmw(color, width, weight);
+        }
+        if (brand.equalsIgnoreCase("hyundai")) {
+            return new Hyundai(color, width, weight);
+        }
+        return null;
+    }
     public static void printAll(Car[] cars) {
         for (int i = 0; i < cars.length; i++) {
             if (cars[i] == null) {
