301f4426a94413ca12414ceb10f47c989963e4b8
LianaSimonian <liana.simoniian@gmail.com>
2022-02-09 11:35:17 +0300

    The solution of the polymorphic exam

---
+++ untitled1/src/com/aca/homework/week5/exam/ExamTest.java
@@ -0,0 +1,30 @@
+package com.aca.homework.week5.exam;
+
+public class ExamTest {
+    private String username;
+    private boolean passed;
+
+    public ExamTest(String username, boolean passed) {
+        this.username = username;
+        this.passed = passed;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean getPassed() {
+        return passed;
+    }
+
+    @Override
+    public String toString() {
+        return "username='" + username + '\'' +
+                ", passed=" + passed;
+    }
+
+    public void print() {
+        System.out.println(toString());
+    }
+
+}
