eca2d58d874d8031431ba55878e785414e0038b0
LianaSimonian <liana.simoniian@gmail.com>
2022-02-13 15:42:55 +0300

    The solution of the light out game

--- untitled1/src/com/aca/homework/week5/light/out/Game.java
+++ untitled1/src/com/aca/homework/week5/light/out/Game.java
@@ -49,7 +49,6 @@ public class Game {
     private String inputCoordinate() {
         while (true) {
             System.out.println("please enter a hit coordinates in '(x,y)' format :");
-
             String inputString = new Scanner(System.in).nextLine();
             if (!isValidCoordinate(Character.getNumericValue(inputString.charAt(1)), Character.getNumericValue(inputString.charAt(3)))) {
                 System.out.println("Input error:please input true coordinate:");
@@ -75,7 +74,7 @@ public class Game {
                 || (i == 2 && j == 4) || (i == 3 && j == 1) || (i == 3 && j == 3) || (i == 4 && j == 2);
     }
 
-    public boolean isValidCoordinate(int x, int y) {
+    private boolean isValidCoordinate(int x, int y) {
         return x >= 0 && x < lights.length && y >= 0 && y < lights[0].length;
     }
 
@@ -84,13 +83,85 @@ public class Game {
         System.out.println("moves: " + moves);
         System.out.println("  12345");
         System.out.println("---------");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < lights.length; i++) {
             System.out.print(i + 1 + "|");
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < lights[i].length; j++) {
                 System.out.print(lights[i][j].getLight());
             }
             System.out.println();
         }
         System.out.println();
     }
+    //if there was an inheritance, its may be to replace private functions with protected
 }
+/*
+  12345
+-------
+1|++0++
+2|+0+0+
+3|0+++0
+4|+0+0+
+5|++0++
+
+
+Create a Light class that can be either in 'on' or 'off' state.
+
+Create a Game class that container matrix of lights of 5x5.
+Create an initialzie() method that will fill the matrix with the light having the above depicted state where + means that the light is 'on' and 0 is 'off'.
+
+To make the Game graphic more understandable fill the board with numbers, |s and -s.
+
+The Game class should have start method that start asking light position in a format '(x,y)' and will switch the light state located at (x,y), (x-1,y), (x+1,y), (x,y-1) and (x,y+1) coordinates.
+
+The game ends when all light are off.
+
+for instance to win the game user should input the following strings
+
+(3,3)
+(1,1)
+(5,5)
+(5,1)
+(1,5)
+
+After each user input the moves count and the updated game graphic should be printed.
+
+The game should be started in the folllowing way:
+----------------------------------------------------------
+The game started!!
+moves: 0
+  12345
+-------
+1|++0++
+2|+0+0+
+3|0+++0
+4|+0+0+
+5|++0++
+
+please enter a hit coordinates in '(x,y)' format:
+(3,3)
+
+moves: 1
+  12345
+-------
+1|++0++
+2|+000+
+3|00000
+4|+000+
+5|++0++
+
+please enter a hit coordinates in '(x,y)' format:
+
+
+
+
+
+
+
+
+.
+.
+.
+
+
+You won!
+ */
