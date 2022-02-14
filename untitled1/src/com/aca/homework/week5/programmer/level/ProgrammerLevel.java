720c27eb14c29fa38c5003e5f7f96a68f53809df
LianaSimonian <liana.simoniian@gmail.com>
2022-02-14 14:57:17 +0300

    The solution of the 'programmer level enum'

--- untitled1/src/com/aca/homework/week5/programmer/level/ProgrammerLevel.java
+++ untitled1/src/com/aca/homework/week5/programmer/level/ProgrammerLevel.java
@@ -17,8 +17,3 @@ public enum ProgrammerLevel {
         throw new RuntimeException();
     }
 }
-
-/*
-Create a ProgrammerLevel enum class that has 3 constants: JUNIOR, MID, SENIOR. Implement the toString method to return
-"Junior programmer", "Mid programmer" and "Senior programmer" for JUNIOR, MID, SENIOR constants respectively.
- */
