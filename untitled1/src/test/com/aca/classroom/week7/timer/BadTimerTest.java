8fa4b4a415a9d59b98c7ef22265b58aad8643fa1
LianaSimonian <liana.simoniian@gmail.com>
2022-02-23 11:35:00 +0300

    the solution of the 'timer'

--- untitled1/src/test/com/aca/classroom/week7/timer/BadTimerTest.java
+++ untitled1/src/test/com/aca/classroom/week7/timer/BadTimerTest.java
@@ -3,7 +3,7 @@ package com.aca.classroom.week7.timer;
 
 class BadTimerTest {
     public static void main(String[] args) {
-        BadTimer badTimer = new BadTimer();
+        Timer badTimer = new BadTimer();
         badTimer.start();
     }
 
