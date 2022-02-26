aa6993a490a1e7ab203585cd94104665b645ca45
LianaSimonian <liana.simoniian@gmail.com>
2022-02-26 21:36:14 +0300

    The solution of 'my integer' problem updated

--- untitled1/src/test/com/aca/homework/week6/myinteger/MyIntegerTest.java
+++ untitled1/src/test/com/aca/homework/week6/myinteger/MyIntegerTest.java
@@ -2,6 +2,7 @@ package com.aca.homework.week6.myinteger;
 
 class MyIntegerTest {
     public static void main(String[] args) {
+
         System.out.println(MyInteger.valueOf(17).getValue());
         System.out.println(MyInteger.valueOf(10).getValue());
         System.out.println(MyInteger.valueOf(17).getValue());
