7974e7bd388d8e8dcacc13ef7eb29f527d2b9360
LianaSimonian <liana.simoniian@gmail.com>
2022-02-27 13:15:18 +0300

    The solution of '2string concat test' problem updated

--- untitled1/src/main/com/aca/classroom/week7/concat/LongConcatenator.java
+++ untitled1/src/main/com/aca/classroom/week7/concat/LongConcatenator.java
@@ -1,17 +1,7 @@
 package com.aca.classroom.week7.concat;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 public class LongConcatenator {
-    private LongConcatenator testSubject;
-    @BeforeEach
-    public void setUp(){
-        testSubject = new LongConcatenator();
+    public String concat(long l1, long l2) {
+        return l1 + "" + l2;
     }
-    public String concat(long l1,long l2){
-
-        return l1+""+l2;
-    }
-
 }
