3f82af4009b9b6ba01ab162fb48a177e277d784e
LianaSimonian <liana.simoniian@gmail.com>
2022-03-06 01:12:01 +0300

    The solution of the 'clickable' problem updated

--- untitled1/src/main/com/aca/homework/week7/clickable/Button.java
+++ untitled1/src/main/com/aca/homework/week7/clickable/Button.java
@@ -1,9 +1,7 @@
 package com.aca.homework.week7.clickable;
 
 public class Button extends AbstractClickable {
-
     public Button(String name, Action action) {
         super(name, action);
     }
-
 }
--- untitled1/src/main/com/aca/homework/week7/clickable/Clickable.java
+++ untitled1/src/main/com/aca/homework/week7/clickable/Clickable.java
@@ -1,7 +1,6 @@
 package com.aca.homework.week7.clickable;
 
 public interface Clickable {
-
     String name();
 
     void click();
