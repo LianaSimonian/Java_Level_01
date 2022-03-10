e9c53b37a13a8f794f30d60370b3270aa4786aba
LianaSimonian <liana.simoniian@gmail.com>
2022-03-10 18:37:12 +0300

    the 'care repository' problem:GitHub error has been corrected , deleted all queue parts

---
+++ untitled1/src/main/com/aca/homework/week8/carrepository/CarRepository.java
@@ -0,0 +1,30 @@
+package com.aca.homework.week8.carrepository;
+
+public class CarRepository {
+    private final int size = 1000;
+    private Car[] cars = new Car[size];
+    private int totalCount;
+
+    public Car save(Car car) {
+        Car updatedCar = findByVin(car.getVin());
+        if (updatedCar != null && car != updatedCar) {
+            updatedCar.setYear(car.getYear());
+            updatedCar.setOwnersCount(car.getOwnersCount());
+            return updatedCar;
+        }
+        cars[totalCount++] = car;
+        return cars[totalCount - 1];
+    }
+
+    public Car findByVin(String vin) {
+        for (int i = 0; i < totalCount; i++) {
+            if (cars[i].getVin().equals(vin))
+                return cars[i];
+        }
+        return null;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+}
---
+++ untitled1/src/test/com/aca/homework/week8/carrepository/CarTest.java
@@ -0,0 +1,27 @@
+package com.aca.homework.week8.carrepository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @Test
+    public void testWhenReferenceIsSame() {
+        Car car = new Car("4Y1SL65848Z411439", 2005, 2);
+        Assertions.assertTrue(car.equals(car));
+    }
+
+    @Test
+    public void testWhenCarsHaveSameFieldValues() {
+        Car car1 = new Car("4Y1SL65848Z411439", 2005, 2);
+        Car car2 = new Car("4Y1SL65848Z411439", 2005, 2);
+        Assertions.assertTrue(car1.equals(car2));
+    }
+
+    @Test
+    public void testCarEqualString() {
+        Car car = new Car("4Y1SL65848Z411439", 2005, 2);
+        Assertions.assertFalse(car.equals("String"));
+    }
+}
