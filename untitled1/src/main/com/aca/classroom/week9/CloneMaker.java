49c13763a500fdf77c0b8d4a50342e09f13bbacd
LianaSimonian <liana.simoniian@gmail.com>
2022-03-10 14:48:13 +0300

    xy

---
+++ untitled1/src/main/com/aca/classroom/week9/CloneMaker.java
@@ -0,0 +1,20 @@
+package com.aca.classroom.week9;
+/*
+public class CloneMaker<T extends ConstrunctorClonable<T>> {
+  // public T doClone(T input){
+       //return  (T)input.clone();
+  // }
+    public T printCloneAndPrint(T  input){
+        System.out.println(input);
+        T newClonedObject = input.doClone();
+        System.out.println(newClonedObject);
+        return newClonedObject;
+    }
+
+    public static void main(String[] args) {
+        CloneMaker<Car>
+    }
+}
+
+
+ */
