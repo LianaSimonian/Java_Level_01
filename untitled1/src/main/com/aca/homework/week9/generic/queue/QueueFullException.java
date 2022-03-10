14f4cd98c8236050f110ef99c2f793ab397134cb
LianaSimonian <97042938+LianaSimonian@users.noreply.github.com>
2022-03-10 15:26:01 +0300

    Delete QueueFullException.java

--- untitled1/src/main/com/aca/homework/week9/generic/queue/QueueFullException.java
+++
@@ -1,9 +0,0 @@
-package com.aca.homework.week9.generic.queue;
-
-
-public class QueueFullException extends RuntimeException {
-    QueueFullException(String message) {
-        super(message);
-    }
-}
-
