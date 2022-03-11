7dc0249ddd9c7343aeae18c47cda38b351b3a807
LianaSimonian <liana.simoniian@gmail.com>
2022-03-11 14:55:12 +0300

    updated: now read function does not throw checked exception in interface InvoiceReader,GitHub does not contain other files not related this task

---
+++ untitled1/src/main/com/aca/homework/week9/iservice/invoice/Invoice.java
@@ -0,0 +1,50 @@
+package com.aca.homework.week9.iservice.invoice;
+
+import java.util.Objects;
+
+public class Invoice {
+    private String id;
+    private int amount;
+    private InvoiceType type;
+    private String code;
+
+    public Invoice(String id, int amount, InvoiceType type, String code) {
+        this.id = id;
+        this.amount = amount;
+        this.type = type;
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "id='" + id + '\'' +
+                ", amount=" + amount +
+                ", type=" + type +
+                ", code='" + code + '\'';
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public InvoiceType getType() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+        Invoice invoice = (Invoice) o;
+        return amount == invoice.amount && Objects.equals(id, invoice.id) && type == invoice.type && Objects.equals(code, invoice.code);
+    }
+}
---
+++ untitled1/src/main/com/aca/homework/week9/iservice/invoice/InvoiceReader.java
@@ -0,0 +1,6 @@
+package com.aca.homework.week9.iservice.invoice;
+
+public interface InvoiceReader {
+    Invoice[] read();
+}
+
