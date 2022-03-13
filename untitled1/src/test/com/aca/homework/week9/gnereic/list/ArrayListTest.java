33b445cdee90ae91b5ea932e94f7e1d2e7a5c968
LianaSimonian <liana.simoniian@gmail.com>
2022-03-13 23:33:11 +0300

    the solution of the ' generic list' problem

---
+++ untitled1/src/test/com/aca/homework/week9/gnereic/list/ArrayListTest.java
@@ -0,0 +1,61 @@
+package com.aca.homework.week9.gnereic.list;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ArrayListTest {
+    @Test
+    public void testSizeForInteger() {
+        List<Integer> testSubject = new ArrayList<>();
+        testSubject.add(Integer.valueOf(12));
+        testSubject.add(new Integer(178));
+        Assertions.assertEquals(2, testSubject.size());
+    }
+
+    @Test
+    public void testSizeForString() {
+        List<String> testSubject = new ArrayList<>();
+        testSubject.add("string1");
+        testSubject.add("string2");
+        Assertions.assertEquals(2, testSubject.size());
+    }
+
+    @Test
+    public void testGetForString() {
+        List<String> testSubject = new ArrayList<>();
+        testSubject.add("string1");
+        testSubject.add("string2");
+        Assertions.assertEquals("string1", testSubject.get(0));
+        Assertions.assertEquals("string2", testSubject.get(1));
+    }
+
+    @Test
+    public void testAddForString() {
+        List<String> testSubject = new ArrayList<>();
+        testSubject.add("string1");
+        testSubject.add("string2");
+        Assertions.assertEquals(2, testSubject.size());
+        Assertions.assertEquals("string1", testSubject.get(0));
+        Assertions.assertEquals("string2", testSubject.get(1));
+    }
+
+    @Test
+    public void testGetForInteger() {
+        List<Integer> testSubject = new ArrayList<>();
+        testSubject.add(Integer.valueOf(1));
+        testSubject.add(Integer.valueOf(2));
+        Assertions.assertEquals(1, testSubject.get(0));
+        Assertions.assertEquals(2, testSubject.get(1));
+    }
+
+    @Test
+    public void testAddForInteger() {
+        List<Integer> testSubject = new ArrayList<>();
+        testSubject.add(Integer.valueOf(3));
+        testSubject.add(Integer.valueOf(4));
+        Assertions.assertEquals(2, testSubject.size());
+        Assertions.assertEquals(3, testSubject.get(0));
+        Assertions.assertEquals(4, testSubject.get(1));
+    }
+
+}
