3e425996cc87ce717997b98ad224160862c46b56
LianaSimonian <liana.simoniian@gmail.com>
2022-03-14 20:27:05 +0300

    class work generics

--- untitled1/src/main/com/aca/classroom/week10/Main.java
+++ untitled1/src/main/com/aca/classroom/week10/Main.java
@@ -25,8 +25,12 @@ public class Main {
         Iterator<Integer> iterator1 = list.iterator();
         while (iterator1.hasNext()) {
             if (iterator1.next() == 4) iterator1.remove();
-
         }
+        for( int i =0;i<list.size();i++)
+            System.out.println(list.get(i));
+
+       // Iterator<Integer> iterator2 = list.iterator();
+        //System.out.println(list);
 
     }
 }
