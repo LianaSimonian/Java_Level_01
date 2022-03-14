5851a20c4247b2178fd87b4b44570276d9edf0d9
LianaSimonian <liana.simoniian@gmail.com>
2022-03-14 19:32:59 +0300

    class work generics

---
+++ untitled1/src/main/com/aca/classroom/week10/Main.java
@@ -0,0 +1,34 @@
+package com.aca.classroom.week10;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(1000);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+        Iterator<Integer> iterator = list.iterator();
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator);
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        for (Integer integer : list) {
+            System.out.println(iterator.next());
+        }
+        Iterator<Integer> iterator1 = list.iterator();
+        while (iterator1.hasNext()) {
+            if (iterator1.next() == 4) iterator1.remove();
+
+        }
+
+    }
+}
+//skeletal implementation
+
