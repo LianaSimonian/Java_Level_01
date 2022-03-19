4107a6c8d47ac0524e944aea9386ec13ee59456a
LianaSimonian <liana.simoniian@gmail.com>
2022-03-19 13:51:22 +0300

    abstract method substituted with  non-abstract method ,added new field for camera description

--- untitled/main/week10/enums/list/PhoneModel.java
+++ untitled/main/week10/enums/list/PhoneModel.java
@@ -3,27 +3,17 @@ package week10.enums.list;
 import java.util.List;
 
 public enum PhoneModel {
-    NOKIA_1100 {
-        @Override
-        public List<String> cameraDescriptionList() {
-            return emptyList;
-        }
-    },
-    NOKIA_3110 {
-        @Override
-        public List<String> cameraDescriptionList() {
-            return listOf1items;
-        }
-    },
-    GALAXY_S2 {
-        @Override
-        public List<String> cameraDescriptionList() {
-            return listOf2items;
-        }
-    };
-    private static final List<String> listOf2items = List.of("back camera", "front camera");
-    private static final List<String> listOf1items = List.of("back camera");
-    private static final List<String> emptyList = List.of();
+    NOKIA_1100(List.of()),
+    NOKIA_3110(List.of("back camera")),
+    GALAXY_S2(List.of("back camera", "front camera"));
 
-    public abstract List<String> cameraDescriptionList();
+    private List<String> descriptionOfCamera;
+
+    PhoneModel(List<String> descriptionOfCamera) {
+        this.descriptionOfCamera = descriptionOfCamera;
+    }
+
+    public List<String> cameraDescriptionList() {
+        return descriptionOfCamera;
+    }
 }
