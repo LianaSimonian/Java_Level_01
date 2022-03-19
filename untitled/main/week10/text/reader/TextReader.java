72bd1d859baae41c390833d3ca48e98d199040f8
LianaSimonian <liana.simoniian@gmail.com>
2022-03-19 13:15:38 +0300

    ArrayList substituted with LinkedList

--- untitled/main/week10/text/reader/TextReader.java
+++ untitled/main/week10/text/reader/TextReader.java
@@ -3,12 +3,13 @@ package week10.text.reader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class TextReader {
     public static List<String> readLines() throws FileNotFoundException {
-        List<String> stringList = new ArrayList<>();
+        List<String> stringList = new LinkedList<>();
         Scanner scanner = new Scanner(new File("D:\\Users\\user\\IdeaProjects\\Java_Level_01\\untitled\\main\\com\\aca\\homework\\week10\\text\\reader\\doc_numbers.txt"));
         while (scanner.hasNextLine())
             stringList.add(scanner.nextLine());
@@ -16,10 +17,12 @@ public class TextReader {
         return stringList;
     }
 
-    public static void print(List<String> stringList) {
-        for (int i = 0; i < stringList.size(); i++)
-            System.out.println(" index number:" + i + ", " + "item: " + stringList.get(i));
-
+    public static <T> void print(List<T> stringList) {
+        int i = 0;
+        for (T item : stringList) {
+            System.out.println(" index number:" + i + ", " + "item: " + item);
+            i++;
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
