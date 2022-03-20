41dd910dcb4c899aba2990deebeff513fcc2e15e
LianaSimonian <liana.simoniian@gmail.com>
2022-03-20 13:26:30 +0300

    List,Iterator

--- untitled1/src/main/com/aca/classroom/week10/Main.java
+++ untitled1/src/main/com/aca/classroom/week10/Main.java
@@ -13,26 +13,28 @@ public class Main {
 
         System.out.println(list);
         Iterator<Integer> iterator = list.iterator();
-        System.out.println(iterator.next());
-        System.out.println(iterator.next());
         System.out.println(iterator);
+       /*
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        */
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
+        /*
         for (Integer integer : list) {
-            System.out.println(iterator.next());
+            System.out.println(iterator.next());//?behavior
         }
+        */
         Iterator<Integer> iterator1 = list.iterator();
-        while (iterator1.hasNext()) {
-            if (iterator1.next() == 4) iterator1.remove();
-        }
-        for( int i =0;i<list.size();i++)
-            System.out.println(list.get(i));
 
-       // Iterator<Integer> iterator2 = list.iterator();
-        //System.out.println(list);
+        while (iterator1.hasNext()) {
+            if (iterator1.next() == 3) iterator1.remove();
+        }
+        for (int i = 0; i < list.size(); i++)
+            System.out.println(list.get(i));
 
     }
 }
-//skeletal implementation
-
+//skeletal implementation class: ArrayList(Effective Java Item 18)
--- untitled1/src/main/com/aca/classroom/week10/MainProject.java
+++ untitled1/src/main/com/aca/classroom/week10/MainProject.java
@@ -3,20 +3,25 @@ package com.aca.classroom.week10;
 
 import java.util.*;
 
-public class MainPeroject {
+public class MainProject {
     public static void main(String[] args) {
         List<Integer> list = new LinkedList<>();//ArrayList
         for (int i = 0; i < 100_000; i++)
             list.add(i);
         printAll(list);
         List<Integer> list3 = Collections.emptyList();
+        List<Integer> list4 = Collections.singletonList(12);
+        List<Integer> list5 = List.of();
     }
 
     //shift//shift-enter
     public static void printAll(List<Integer> list) {
         long start = System.currentTimeMillis();
-        // for( int i =0;i<list.size();i++)
-        //    System.out.println(list.get(i));
+        /*
+         for( int i =0;i<list.size();i++)
+            System.out.println(list.get(i));
+
+         */
         for (Integer integer : list)
             System.out.println(integer);
         System.out.println(System.currentTimeMillis() - start);
