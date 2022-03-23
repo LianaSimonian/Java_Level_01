ff7fdff90ba5c78e64d08e8eeb5f67b9beb6361b
LianaSimonian <liana.simoniian@gmail.com>
2022-03-23 12:58:26 +0300

    the solution of the 'hello printer' problem

---
+++ untitled/main/com/aca/homework/week11/resolver/HelloTranslaterResolver.java
@@ -0,0 +1,19 @@
+package com.aca.homework.week11.resolver;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HelloTranslaterResolver {
+    private Map<LanguageType, HelloTranslater> helloTranslaters = new HashMap<>();
+
+    HelloTranslaterResolver(List<HelloTranslater> list) {
+        for (HelloTranslater helloTranslater : list) {
+            helloTranslaters.put(helloTranslater.type(), helloTranslater);
+        }
+    }
+
+    HelloTranslater resolve(LanguageType type) {
+        return helloTranslaters.get(type);
+    }
+}
