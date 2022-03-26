e28a6424c20d75459888cb32b47cf92afb0a8032
LianaSimonian <liana.simoniian@gmail.com>
2022-03-26 19:53:55 +0300

    the solution of the 'printer runnable' problem

---
+++ untitled/main/com/aca/homework/week11/sorted/runnable/printer/Main.java
@@ -0,0 +1,7 @@
+package com.aca.homework.week11.sorted.runnable.printer;
+
+public class Main {
+    public static void main(String[] args) {
+        SortedNumberPrinterRunnable.runAll(SortedNumberPrinterRunnable.createRunnables());
+    }
+}
