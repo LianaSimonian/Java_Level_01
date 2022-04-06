d145f953dc4f545ceda848b4648061729d309e21
LianaSimonian <liana.simoniian@gmail.com>
2022-04-06 10:12:43 +0300

    the solution of the 'runnable stop' classroom version

--- untitled/main/com/aca/homework/week13/race/condition/list/NumberAdderRunnable.java
+++ untitled/main/com/aca/homework/week13/race/condition/list/NumberAdderRunnable.java
@@ -2,8 +2,8 @@ package com.aca.homework.week13.race.condition.list;
 
 public class NumberAdderRunnable implements Runnable {
 
-    List<Integer> list;
-    Integer integer;
+    private List<Integer> list;
+    private Integer integer;
 
     public NumberAdderRunnable(List<Integer> list, Integer integer) {
         this.list = list;
