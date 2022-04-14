70719f74e7abec1efe90739e026b6be8df066387
LianaSimonian <liana.simoniian@gmail.com>
2022-04-14 20:03:49 +0300

    the solution of the 'parallel file timer' problem

---
+++ untitled/src/main/java/com/aca/homework/week14/parallel/file/timer/TimerRunnable.java
@@ -0,0 +1,54 @@
+package com.aca.homework.week14.parallel.file.timer;
+
+import java.io.*;
+
+public class TimerRunnable implements Runnable {
+
+    private final static String PATH = "D:\\Users\\user\\IdeaProjects\\aca2\\Java_Level_01\\untitled\\src\\main\\java\\com\\aca\\homework\\week14\\parallel\\file\\timer\\";
+    private final String filename;
+    private boolean isRunning = true;
+
+    TimerRunnable(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void run() {
+        long start = System.currentTimeMillis();
+
+        while (isRunning) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            ObjectOutputStream objectOutputStream = null;
+            try {
+                objectOutputStream = new ObjectOutputStream(new FileOutputStream(PATH + filename + ".txt"));
+            } catch (IOException e) {
+                throw new RuntimeException("Error trying to write in file with path: " + PATH + filename + ".txt", e);
+            }
+
+            try {
+                objectOutputStream.writeLong((System.currentTimeMillis() - start) / 1000);
+            } catch (IOException e) {
+                throw new RuntimeException("write error in file with path: " + PATH + filename + ".txt", e);
+            }
+
+            try {
+                objectOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stop() {
+        isRunning = false;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+}
---
+++ untitled/src/test/java/com/aca/homework/week14/parallel/file/timer/TimerMainTest.java
@@ -0,0 +1,78 @@
+package com.aca.homework.week14.parallel.file.timer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+class TimerMainTest {
+
+    @Test
+    public void test5And3() {
+        TimerMain testSubject = new TimerMain(new Supplier<String>() {
+            private String[] strings = {"timer1", "timer2", "timer1", "timer2"};
+            private int counter;
+
+            @Override
+            public String get() {
+                if (counter == 0) {
+                    return strings[counter++];
+                }
+                if (counter == 1) {
+                    try {
+                        Thread.sleep(2500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return strings[counter++];
+                }
+                if (counter == 2) {
+                    try {
+                        Thread.sleep(3200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return strings[counter++];
+                }
+                if (counter == 3)
+                    return strings[counter];
+                return "";
+            }
+        });
+        testSubject.startTimer();
+        Assertions.assertEquals(5, TimerMain.getSecondsFromFile("timer1"));
+        Assertions.assertEquals(3, TimerMain.getSecondsFromFile("timer2"));
+    }
+
+    @Test
+    public void test5End5() {
+        TimerMain testSubject = new TimerMain(new Supplier<String>() {
+            private String[] strings = {"timer1", "timer2", "timer1", "timer2"};
+            private int counter;
+
+            @Override
+            public String get() {
+                if (counter == 0) {
+                    return strings[counter++];
+                }
+                if (counter == 1) {
+                    return strings[counter++];
+                }
+                if (counter == 2) {
+                    try {
+                        Thread.sleep(5200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return strings[counter++];
+                }
+                if (counter == 3)
+                    return strings[counter];
+                return "";
+            }
+        });
+        testSubject.startTimer();
+        Assertions.assertEquals(5, TimerMain.getSecondsFromFile("timer1"));
+        Assertions.assertEquals(5, TimerMain.getSecondsFromFile("timer2"));
+    }
+}
