78bcad0066afa83936c53d383a37e544ab89fcb4
LianaSimonian <liana.simoniian@gmail.com>
2022-04-17 15:07:27 +0300

    the solution of the 'SQL generator' problem

---
+++ untitled/src/main/java/com/aca/homework/week15/sql/generate/User.java
@@ -0,0 +1,16 @@
+package com.aca.homework.week15.sql.generate;
+
+@Table(name = "users")
+public class User {
+
+    @Column(name = "full_name")
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
