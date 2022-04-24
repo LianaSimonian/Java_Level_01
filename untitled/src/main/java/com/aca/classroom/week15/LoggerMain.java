75cca608b849c3ad979d2de6c1bc9c9b9f1954cf
LianaSimonian <liana.simoniian@gmail.com>
2022-04-24 21:30:00 +0300

    Logger

---
+++ untitled/src/main/java/com/aca/classroom/week15/LoggerMain.java
@@ -0,0 +1,17 @@
+package com.aca.classroom.week15;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerMain {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggerMain.class);
+
+    public static void main(String[] args) {
+        max(5, 10);
+    }
+
+    public static int max(int a, int b) {
+        LOGGER.debug("Calculating maximum between numbers a {} and b {}", a, b);
+        return a > b ? a : b;
+    }
+}
