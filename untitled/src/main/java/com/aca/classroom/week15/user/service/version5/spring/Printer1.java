1f4402385ad70d1498658d93b3b2af5677f9b074
LianaSimonian <liana.simoniian@gmail.com>
2022-05-18 13:14:03 +0300

    updated

--- untitled/src/main/java/com/aca/classroom/week15/user/service/version5/spring/Printer1.java
+++ untitled/src/main/java/com/aca/classroom/week15/user/service/version5/spring/Printer1.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component(value = "defaultPrinter")
 public class Printer1 implements Printer {
+
     @Override
     public void print() {
         System.out.println("printer1");
